5e24be299f5f3f376066c6da9d91f4e0db1bf3fa
Fumiki Yoshioka <fumikistrider@gmail.com>
2023-01-28 14:46:59 +0900

    app layout fix

--- app/mySketch.js
+++ app/mySketch.js
@@ -16,6 +16,7 @@ let btn_send;
 let btn_erase;
 let btn_pen1;
 let btn_pen2;
+let btn_help;
 
 // Images
 let img_bg;
@@ -36,8 +37,8 @@ function preload(){
 //
 function setup() {
 	print(socket);
-  createCanvas(windowWidth, windowHeight);
-  background(img_bg);
+	createCanvas(windowWidth, windowHeight);
+	background(img_bg);
 	colorMode(HSB,100,100,100,100);
 
 	// 参加
@@ -46,40 +47,43 @@ function setup() {
 
 	// UI
 	btn_send = createImg('sp_btn_send.png','送信する');
-	btn_send.size(220,100);
-	btn_send.position(width * 0.20, height * 0.80);
+	btn_send.size(330,180);
+	btn_send.position(width * 0.33, height * 0.80);
 	btn_send.mouseReleased(buttonPressed);
-	
+
 	btn_erase = createImg('sp_btn_erase.png','消す');
-	btn_erase.size(54,36);
-	btn_erase.position(width * 0.05, height * 0.85);
+	btn_erase.size(162,108);
+	btn_erase.position(width * 0.05, height * 0.825);
 	btn_erase.mouseReleased(erasePressed);
-	
+
 	btn_pen1 = createImg('pen1.png','ペン1');
-	btn_pen1.size(40,80);
+	btn_pen1.size(80,160);
 	btn_pen1.position(width * 0.70, height * 0.70);
 	btn_pen1.mouseReleased(pen1Pressed);
 
 	btn_pen2 = createImg('pen2.png','ペン2');
-	btn_pen2.size(20,40);
+	btn_pen2.size(40,80);
 	btn_pen2.position(width * 0.85, height * 0.75);
 	btn_pen2.mouseReleased(pen2Pressed);
-	
-	
+
+	btn_help = createImg('help.png','ヘルプ');
+	btn_help.size(100,100);
+	btn_help.position(width*0.75, height * 0.825);
+
 }
 
 function pen1Pressed(){
-	btn_pen1.size(40,80);
+	btn_pen1.size(80,160);
 	btn_pen1.position(width * 0.70, height * 0.70);
-	btn_pen2.size(20,40);
+	btn_pen2.size(40,80);
 	btn_pen2.position(width * 0.85, height * 0.75);
 	penWeight = 1.5;
 }
 
 function pen2Pressed(){
-	btn_pen1.size(20,40);
+	btn_pen1.size(40,80);
 	btn_pen1.position(width * 0.70, height * 0.75);
-	btn_pen2.size(40,80);
+	btn_pen2.size(80,160);
 	btn_pen2.position(width * 0.85, height * 0.70);
 	penWeight = 5;
 }
@@ -92,7 +96,7 @@ function draw() {
 	fill(30,30,30,90);
 	push();
 	translate(width/2,height*0.07);
-	scale(0.8);
+	scale(1.5);
 	imageMode(CENTER);
 	image(img_title,0,0);
 	pop();
