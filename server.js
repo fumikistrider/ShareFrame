73a5ea3e128f9a11fecfeab404a0ab1cebbacf92
Fumiki Yoshioka <fumikistrider@gmail.com>
2023-03-06 19:13:10 +0900

    戻し

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var fs = require('fs');
 var http = require('http');
 var server = http.createServer();
 
-// server.on('request', function(req, res) {
-//   var stream = fs.createReadStream('index.html');
-//   res.writeHead(200, {'Content-Type': 'text/html'});
-//   stream.pipe(res);
-// });
+server.on('request', function(req, res) {
+  var stream = fs.createReadStream('index.html');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  stream.pipe(res);
+});
 var io = require('socket.io')(server);
 server.listen(30000);
 
